9881ac9a5a7f84d764f6742d6dad05654de41c5c
Schlichting <schl0159@STTHOMAS.EDU>
2018-02-14 10:45:36 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -8,19 +8,71 @@ app.config(function($stateProvider) {
 
 }).controller("appCtrl", function($scope, $state) {
 
+  $scope.lowElement = null;
+  $scope.middleElement = null;
+  $scope.highElement = null;
+
+  $scope.currentGuess = null;
+  $scope.number = -1;
+  $scope.lowGuess = 0;
+  $scope.highGuess = 100;
+
+  $scope.guesses = [];
+
+
     $scope.init = function (){
-        console.log("test");
+        console.log("Version 0.0.3");
+
+        $scope.lowElement = angular.element('#low');
+        $scope.middleElement = angular.element('#middle');
+        $scope.highElement = angular.element('#high');
+
+        $scope.number = 1;//(Math.floor(Math.random() * 98) + 1);
     };
 
+    $scope.guess = function(){
+        if(isNaN($scope.currentGuess) || $scope.currentGuess <= $scope.lowGuess || $scope.currentGuess >= $scope.highGuess){
+          alert('Please Enter a Number between ' + $scope.lowGuess + '-' + $scope.highGuess + '. You entered: \'' + $scope.currentGuess + '\'');
 
-    $scope.number = 50;
-    $scope.lowGuess = 0;
-    $scope.highGuess = 100;
+          console.log('is nan: ' + isNaN($scope.currentGuess));
+          console.log('Number: ' + $scope.number);
+        }
+        else{
+          //is valid guess
+          if($scope.currentGuess == $scope.number){
+            alert('You win! The number was ' + $scope.number + ' You won in ' + ($scope.guesses.length + 1) + ($scope.guesses.length === 0 ? ' guess!' : ' guesses!'));
+            $scope.middleElement.html($scope.number);
 
-    $scope.guesses = [];
+          }
+          else if($scope.currentGuess < $scope.number){
+            $scope.adjustLow($scope.currentGuess);
+          }
+          else{
+            $scope.adjustHigh($scope.currentGuess);
+          }
+        }
 
-    $scope.guess = function(number){
-        alert(number);
+        $scope.currentGuess = "";
     }
 
+
+
+    $scope.adjustLow = function(guess){
+      $scope.lowElement.html(guess);
+      $scope.lowGuess = guess;
+
+      $scope.lowElement.css('width', (guess) + '%');
+      $scope.middleElement.css('width', 94-(100-$scope.highGuess)-$scope.lowGuess+ '%');
+
+      $scope.guesses.push(guess);
+    }
+    $scope.adjustHigh = function(guess){
+      $scope.highElement.html(guess);
+      $scope.highGuess = guess;
+
+      $scope.highElement.css('width', (100-guess) + '%');
+      $scope.middleElement.css('width', 94-(100-$scope.highGuess)-$scope.lowGuess+ '%');
+
+      $scope.guesses.push(guess);
+    }
 });
